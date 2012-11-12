8e626c467d90153fde45e468ebd961e8c9f535be
Eddy Rene Cordón Leiva <eddytrex@gmail.com>
2012-11-11 18:04:58 -0600

    done Get Values form file

--- src/ANN/ANN.go
+++ src/ANN/ANN.go
@@ -4,15 +4,54 @@ import(
 )
 
 type ANN struct{
-  Layers []Matrix.matrix
- 
+  Layers [] Matrix.Matrix
+  Inputs int
+  Activation func(float64)float64
 }
 
-func  InitANN() *ANN{
+
+func CreateANN(NeuronsByLayer []int,Inputs int,Act func(float64)float64) ANN{
+  var out ANN
+  out.Layers=make([]Matrix.Matrix,len(NeuronsByLayer),len(NeuronsByLayer))
   
-  return &ANN
+  out.Inputs=Inputs
+  out.Activation=Act
+  
+  m:=Inputs
+  for i:=0;i<(len(NeuronsByLayer));i++{
+    
+    n:=NeuronsByLayer[i]
+    temp:=Matrix.NullMatrix(m+1,n)
+    out.Layers[i]=temp
+    m=n
+    
+  }
+  return out
 }
 
-func (this *ANN)ForwardPropagation(in matrix)(*matrix){
-  
+
+
+func (this *ANN)ForwardPropagation(In Matrix.Matrix)(Output *Matrix.Matrix){
+  if(In.GetNRows()==1&&In.GetNRows()==this.Inputs){
+	
+	sTemp:=In.Copy()
+	
+	sTemp=sTemp.AddColumn(*Matrix.I(1))//Add  a new row for a Gain Weight 
+	
+	for i:=0;i<len(this.Layers);i++{
+	  
+	  sTemp=Matrix.Product(*sTemp,(this.Layers[i]))
+	  
+	  if(i<len(this.Layers)-1){
+	  sTemp=sTemp.AddColumn(*Matrix.I(1))	//Add  a new row for a Gain Weight 
+	  }
+	 
+	  sTemp=sTemp.Apply(this.Activation) //apply the functions of activation
+	}
+	Output=sTemp
+	return
+  }	  
+  return nil
 }
+
+
