1322bd9c9bd71f261c64d6ce8285c719308dfb71
Eddy Rene Cordón Leiva <eddytrex@gmail.com>
2012-11-22 17:14:21 -0600

    LinearRegression and LogisticRegression

--- src/test.go
+++ src/test.go
@@ -5,6 +5,7 @@ import (
   "Matrix"
   "MachineLearning"
   //"ANN"
+  //"math"
   
 )
 
@@ -12,19 +13,31 @@ import (
 func main(){
  
   
-  a,er1:=Matrix.FromFile("/home/eddytrex/go/IAgo/src/xs.txt")
+  a,er1:=Matrix.FromFile("/home/eddytrex/go/IAgo/src/xsl.txt")
   
-  b,er2:=Matrix.FromFile("/home/eddytrex/go/IAgo/src/ys.txt")
+  b,er2:=Matrix.FromFile("/home/eddytrex/go/IAgo/src/ysl.txt")
   
   fmt.Println(er1,"\n",er2)
   
-  c,_:=Matrix.Sum(*a,*b)
-  fmt.Println(c.ToString(),">")
+  //c:=a.Transpose()
+  //fmt.Println(c.ToString(),">")
   
   //fmt.Println(b.ToString())
   ts:=MachineLearning.MakeTrainingSet(*a,*b)
   
-  hy:=MachineLearning.GradientDescent(0.0001,0.00000001,*ts)
+  //hy:=MachineLearning.GradientDescent(0.0001,0.00000001,*ts,func (a float64)float64{return a})
   
-  fmt.Println("->",hy)
+  //hy:=MachineLearning.LinearRegression(0.0001,0.0000001,*ts)
+  hy:=MachineLearning.LogisticRegression(0.001,0.000001,*ts)
+  
+  fmt.Println("--->",hy)
+  
+  t:=Matrix.NullMatrix(1,2)
+  
+  t.SetValue(1,1,7)
+  t.SetValue(1,2,7)
+  
+  ret,_:=hy.Evaluate(&t)
+ 
+  fmt.Println("h()=",ret)
 }
